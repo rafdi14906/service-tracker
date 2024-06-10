9c5b3d8897d5233868a3e5a6c6d4818df73d23e7
hendryrafdi25 <105276011+hendryrafdi25@users.noreply.github.com>
2024-06-10 09:41:35 +0700

    redirect to dashboard

--- routes/web.php
+++ routes/web.php
@@ -4,17 +4,10 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ServiceLogController;
 use App\Http\Controllers\VehicleController;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    return redirect()->route('dashboard');
 });
 
 Route::middleware(['auth', 'verified'])->group(function() {
